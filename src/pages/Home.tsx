e430609f2f79c8f452407537cbb36525deea90f2
nimrah <nimrahqureshi013@gmail.com>
2026-06-18 16:35:49 +0500

    Clean up Home component by removing unused sections
    Removed unused section imports from Home component.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,6 @@
 import SEOHead from '@/components/seo/SEOHead';
 import Hero from '@/components/home/Hero';
 import Stats from '@/components/home/Stats';
-import AboutSection from '@/components/about/AboutSection';
-import ServicesSection from '@/components/services/ServicesSection';
-import PortfolioSection from '@/components/portfolio/PortfolioSection';
-import CaseStudiesSection from '@/components/case-studies/CaseStudiesSection';
-import AIToolsSection from '@/components/ai-tools/AIToolsSection';
-import TestimonialsSection from '@/components/testimonials/TestimonialsSection';
-import BlogSection from '@/components/blog/BlogSection';
-import PricingSection from '@/components/pricing/PricingSection';
-import ContactSection from '@/components/contact/ContactSection';
 
 export default function Home() {
   return (
@@ -21,11 +12,6 @@ export default function Home() {
       />
       <Hero />
       <Stats />
-      <AIToolsSection />
-      <TestimonialsSection />
-      <BlogSection />
-      <PricingSection />
-      <ContactSection />
     </>
   );
 }
